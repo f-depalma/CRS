a8dc82c43c511cc20c22a0dd5303577316282f74
f.depalma <325587@via.dk>
2022-11-30 20:04:12 +0100

    change database

--- init.sql
+++ init.sql
@@ -3,6 +3,10 @@ SET SCHEMA 'crs';
 
 -- drop table profile;
 -- drop table app_user;
+-- drop table favorite_course;
+-- drop table course;
+-- drop table program;
+
 
 CREATE TABLE app_user
 (
@@ -27,6 +31,31 @@ CREATE TABLE profile
     FOREIGN KEY (id) REFERENCES app_user (id)
 );
 
+CREATE TABLE program
+(
+    short_name VARCHAR(10),
+    name       VARCHAR(50),
+    PRIMARY KEY (short_name)
+);
+
+CREATE TABLE course
+(
+    short_name   VARCHAR(10),
+    name         VARCHAR(50),
+    program_name VARCHAR(10),
+    PRIMARY KEY (short_name),
+    FOREIGN KEY (program_name) REFERENCES program (short_name)
+);
+
+CREATE TABLE favorite_course
+(
+    profile_id  INTEGER,
+    course_name VARCHAR(10),
+    FOREIGN KEY (profile_id) REFERENCES profile (id),
+    FOREIGN KEY (course_name) REFERENCES course (short_name),
+    PRIMARY KEY (profile_id, course_name)
+);
+
 -- INSERT ADMIN ACCOUNT
 INSERT INTO app_user(username, password)
 VALUES ('admin', 'adminadmin');
@@ -35,7 +64,23 @@ INSERT INTO profile(id, first_name, last_name, email, birthday, type)
 VALUES ((select id from app_user where username = 'admin' and password = 'adminadmin'),
         'Ad', 'min', 'admin@admin.ad', '1993-06-17', 'A');
 
+INSERT INTO program(short_name, name)
+VALUES ('IT', 'Software Technology Engineering');
 
-select * from app_user;
-select * from profile;
+INSERT INTO course (short_name, name, program_name)
+VALUES ('SEP2', 'Semester Project 2', 'IT'),
+       ('SDJ2', 'Software Development with UML and Java 2', 'IT');
+
+INSERT INTO favorite_course (profile_id, course_name)
+VALUES (1, 'SEP2');
+
+
+select *
+from app_user;
+
+select *
+from profile;
+
+select *
+from course;
 
